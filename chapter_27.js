49a526a255816d1304ffc20fe2e825b868b4ba17
Nabeel <nabeel.sohail2630@outlook.com>
2023-09-27 11:05:12 +0300

    27

---
+++ chapter_27.js
@@ -0,0 +1,7 @@
+var randomNumber = Math.floor(Math.random() * 50) + 1;
+
+var randomValue = Math.random();
+
+var diceRoll = Math.floor(Math.random() * 6) + 1;
+
+var coinToss = Math.random() < 0.5 ? "Heads" : "Tails";
